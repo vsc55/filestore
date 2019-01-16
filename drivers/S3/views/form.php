5ff10e4e9b41afcb2f15c73960b341c11efb671f
James Finstrom <jfinstrom@sangoma.com>
2019-01-16 18:09:21 +0000

    FREEPBX-18651 Update form, add friendly name.
    Some users do use "human friendly" bucket names. Add a friendly name
    option for select lists etc
    
    Added additional AWS regions
    Reference: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.RegionsAndAvailabilityZones.html

--- drivers/S3/views/form.php
+++ drivers/S3/views/form.php
@@ -29,6 +29,19 @@ $fstype = isset($fstype)?$fstype:'auto';
             			</div>
             		</div>
             	</div>
+            	<!--Local Display Name-->
+            	<div class="element-container">
+            		<div class="row">
+            			<div class="form-group">
+            				<div class="col-md-3">
+            					<label class="control-label" for="display"><?php echo _("Local Display Name") ?></label>
+            				</div>
+            				<div class="col-md-9">
+            					<input type="text" class="form-control" id="displayname" name="displayname" value="<?php echo isset($displayname)?displayname:''?>"<?php echo $disabled?>>
+            				</div>
+            			</div>
+            		</div>
+            	</div>
             	<!--Description-->
             	<div class="element-container">
             		<div class="row">

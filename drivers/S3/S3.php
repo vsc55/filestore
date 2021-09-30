02f83d87a27669a0826a30687c75cb13bec20b16
James Finstrom <jfinstrom@clearlyip.com>
2021-09-30 19:54:07 +0000

    FREEPBX-22083 add ability to set custom endpoint
    - Add new advanced tab
    - Allow custom endpoint
    - Allow custom region

--- drivers/S3/S3.php
+++ drivers/S3/S3.php
@@ -1,12 +1,16 @@
 <?php
+
 namespace FreePBX\modules\Filestore\drivers\S3;
+
 use Aws\S3\S3Client;
 use League\Flysystem\AwsS3v3\AwsS3Adapter;
 use League\Flysystem\Filesystem;
 use \FreePBX\modules\Filestore\drivers\FlysystemBase;
 use League\Flysystem\Cached\CachedAdapter;
 use League\Flysystem\Cached\Storage\Memory as MemoryStore;
-class S3 extends FlysystemBase {
+
+class S3 extends FlysystemBase
+{
 	protected static $path = __DIR__;
 	protected static $validKeys = [
 		"awsaccesskey" => '',
@@ -16,43 +20,46 @@ class S3 extends FlysystemBase {
 		"bucket" => '',
 		"region" => '',
 		'immortal' => '',
-		'path' => ''
+		'path' => '',
+		'customendpoint' => '',
+		'customregion' => '',
 	];
 
 	/**
 	 * The display view for non setting items.
 	 * @return string html
 	 */
-	public static function getDisplay($freepbx, $config){
+	public static function getDisplay($freepbx, $config)
+	{
 		$regions =
-		[
-			'US East (Ohio)' => 'us-east-2',
-			'US East (N. Virginia)' => 'us-east-1',
-			'AWS GovCloud (US-East)' => 'us-gov-east-1',
-			'US West (N. California)' => 'us-west-1',
-			'US West (Oregon)' => 'us-west-2',
-			'AWS GovCloud (US)' => 'us-gov-west-1',
-			'Canada (Central)' => 'ca-central-1',
-			'Asia Pacific (Mumbai)' => 'ap-south-1',
-			'Asia Pacific (Osaka-Local)' => 'ap-northeast-3',
-			'Asia Pacific (Seoul)' => 'ap-northeast-2',
-			'Asia Pacific (Singapore)' => 'ap-southeast-1',
-			'Asia Pacific (Sydney)' => 'ap-southeast-2',
-			'Asia Pacific (Tokyo)' => 'ap-northeast-1',
-			'China (Beijing)' => 'cn-north-1',
-			'China (Ningxia)' => 'cn-northwest-1',
-			'EU (Frankfurt)' => 'eu-central-1',
-			'EU (Ireland)' => 'eu-west-1',
-			'EU (London)' => 'eu-west-2',
-			'EU (Paris)' => 'eu-west-3',
-			'EU (Stockholm)' => 'eu-north-1	',
-			'South America (São Paulo)' => 'sa-east-1',
-		];
-		if(empty($_GET['view'])){
-			return load_view(__DIR__.'/views/grid.php');
-		}else{
+			[
+				'US East (Ohio)' => 'us-east-2',
+				'US East (N. Virginia)' => 'us-east-1',
+				'AWS GovCloud (US-East)' => 'us-gov-east-1',
+				'US West (N. California)' => 'us-west-1',
+				'US West (Oregon)' => 'us-west-2',
+				'AWS GovCloud (US)' => 'us-gov-west-1',
+				'Canada (Central)' => 'ca-central-1',
+				'Asia Pacific (Mumbai)' => 'ap-south-1',
+				'Asia Pacific (Osaka-Local)' => 'ap-northeast-3',
+				'Asia Pacific (Seoul)' => 'ap-northeast-2',
+				'Asia Pacific (Singapore)' => 'ap-southeast-1',
+				'Asia Pacific (Sydney)' => 'ap-southeast-2',
+				'Asia Pacific (Tokyo)' => 'ap-northeast-1',
+				'China (Beijing)' => 'cn-north-1',
+				'China (Ningxia)' => 'cn-northwest-1',
+				'EU (Frankfurt)' => 'eu-central-1',
+				'EU (Ireland)' => 'eu-west-1',
+				'EU (London)' => 'eu-west-2',
+				'EU (Paris)' => 'eu-west-3',
+				'EU (Stockholm)' => 'eu-north-1	',
+				'South America (São Paulo)' => 'sa-east-1',
+			];
+		if (empty($_GET['view'])) {
+			return load_view(__DIR__ . '/views/grid.php');
+		} else {
 			$config['regions'] = $regions;
-			return load_view(__DIR__.'/views/form.php',$config);
+			return load_view(__DIR__ . '/views/form.php', $config);
 		}
 	}
 
@@ -61,7 +68,8 @@ class S3 extends FlysystemBase {
 	 * @param  string $method the method "all,backup,readonly,writeonly"
 	 * @return bool method is/not supported
 	 */
-	public function methodSupported($method){
+	public function methodSupported($method)
+	{
 		$permissions = array(
 			'all',
 			'read',
@@ -74,18 +82,27 @@ class S3 extends FlysystemBase {
 
 
 	//S3 STUFF
-	public function getHandler(){
-		if(isset($this->handler)){
+	public function getHandler()
+	{
+		if (isset($this->handler)) {
 			return $this->handler;
 		}
-		$client = new S3Client([
+		$region = !empty($this->config['customregion']) ? $this->config['customregion'] : $this->config['region'];
+
+		$config = [
 			'credentials' => [
 				'key'    => trim($this->config['awsaccesskey']),
 				'secret' => trim($this->config['awssecret'])
 			],
-			'region' => $this->config['region'],
+			'region' => $region,
 			'version' => 'latest'
-		]);
+		];
+		/** Set an endpoint if the user has specified one. */
+		if (!empty($this->config['customendpoint'])) {
+			$config['endpoint'] = $this->config['customendpoint'];
+		}
+
+		$client = new S3Client($config);
 
 		// Decorate the adapter
 		$adapter = new CachedAdapter(new AwsS3Adapter($client, $this->config['bucket'], $this->config['path']), new MemoryStore());
--- drivers/S3/views/form.php
+++ drivers/S3/views/form.php
@@ -1,159 +1,216 @@
 <?php
-$disabled = (isset($readonly) && !empty($readonly))?' disabled ':'';
-$id = isset($_GET['id'])?$_GET['id']:'';
-if(empty($displayname)){
+$disabled = (isset($readonly) && !empty($readonly)) ? ' disabled ' : '';
+$id = isset($_GET['id']) ? $_GET['id'] : '';
+if (empty($displayname)) {
 	$displayname = $bucket;
 }
-$fstype = isset($fstype)?$fstype:'auto';
+$fstype = isset($fstype) ? $fstype : 'auto';
 ?>
 <div class="container-fluid">
-	<h1><?php echo _('For Use With AWS S3')?></h1>
-	<div class = "display full-border">
+	<h1><?php echo _('For Use With AWS S3') ?></h1>
+	<div class="display full-border">
 		<div class="row">
 			<div class="col-sm-12">
 				<div class="fpbx-container">
 					<div class="display full-border">
-            <form class="fpbx-submit" action="?display=filestore" method="post" id="server_form" name="server_form" data-fpbx-delete="?display=filestore&action=delete&id=<?php echo isset($_GET['id']) ? $_GET['id'] : '' ?>">
-            	<input type="hidden" name="action" value="<?php echo empty($id)?'add':'edit'?>">
-            	<input type="hidden" name="id" value="<?php echo $id?>">
-            	<input type="hidden" name="driver" value="S3">
-							<!--Local Display Name-->
-							<div class="element-container">
-            		<div class="row">
-            			<div class="form-group">
-            				<div class="col-md-3">
-            					<label class="control-label" for="name"><?php echo _("Local Display Name") ?></label>
-            				</div>
-            				<div class="col-md-9">
-            					<input type="text" class="form-control" id="name" name="name" value="<?php echo isset($name)?$name:''?>"<?php echo $disabled?>>
-            				</div>
-            			</div>
-            		</div>
-            	</div>
-            	<!--Bucket Name-->
-            	<div class="element-container">
-            		<div class="row">
-            			<div class="form-group">
-            				<div class="col-md-3">
-            					<label class="control-label" for="bucket"><?php echo _("Bucket Name") ?></label>
-            				</div>
-            				<div class="col-md-9">
-            					<input type="text" class="form-control" id="bucket" name="bucket" value="<?php echo isset($bucket)?$bucket:''?>"<?php echo $disabled?>>
-            				</div>
-            			</div>
-            		</div>
-            	</div>
-            	<!--Description-->
-            	<div class="element-container">
-            		<div class="row">
-            			<div class="form-group">
-            				<div class="col-md-3">
-            					<label class="control-label" for="desc"><?php echo _("Description") ?></label>
-            				</div>
-            				<div class="col-md-9">
-            					<input type="text" class="form-control" id="desc" name="desc" value="<?php echo isset($desc)?$desc:''?>"<?php echo $disabled?>>
-            				</div>
-            			</div>
-            		</div>
-            	</div>
-            	<!--END Description-->
-              <!--AWS Region-->
-              <div class="element-container">
-                <div class="row">
-                  <div class="form-group">
-                    <div class="col-md-3">
-                      <label class="control-label" for="region"><?php echo _("AWS Region") ?></label>
-                      <i class="fa fa-question-circle fpbx-help-icon" data-for="region"></i>
-                    </div>
-                    <div class="col-md-9">
-                        <select class="form-control" id="region" name="region">
-												<?php
-                          foreach ($regions as $value => $key) {
-                            $selected = ( $key == $region)?'SELECTED':'';
-                            echo '<option value = "'.$key.'" '.$selected.'>'.$value.' ['.$key.']</option>';
-                          }
-                        ?>
-                        </select>
-                    </div>
-                    </div>
-                  </div>
-                <div class="row">
-                  <div class="col-md-12">
-                    <span id="region-help" class="help-block fpbx-help-block"><?php echo _("Region provided in AWS")?></span>
-                  </div>
-                </div>
-              </div>
-              <!--END AWS Region-->
-            	<!--AWS Access Key-->
-            	<div class="element-container">
-            		<div class="row">
-            			<div class="form-group">
-            				<div class="col-md-3">
-            					<label class="control-label" for="awsaccesskey"><?php echo _("AWS Access Key") ?></label>
-            				</div>
-            				<div class="col-md-9">
-            					<div class="input-group">
-            						<input type="text" class="form-control" id="awsaccesskey" name="awsaccesskey" value="<?php echo isset($awsaccesskey)?$awsaccesskey:''?>"<?php echo $disabled?>>
-            						<span class="input-group-addon" id="awskeyaddon"><a href="http://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSGettingStartedGuide/AWSCredentials.html" target="_blank"><?php echo _("What's this?")?></a></span>
-            					</div>
-            				</div>
-            			</div>
-            		</div>
-            	</div>
-            	<!--END AWS Access Key-->
-            	<!--AWS Secret-->
-            	<div class="element-container">
-            		<div class="row">
-            			<div class="form-group">
-            				<div class="col-md-3">
-            					<label class="control-label" for="awssecret"><?php echo _("AWS Secret") ?></label>
-            				</div>
-            				<div class="col-md-9">
-            					<div class="input-group">
-            						<input type="text" class="form-control" id="awssecret" name="awssecret" value="<?php echo isset($awssecret)?$awssecret:''?>"<?php echo $disabled?>>
-            						<span class="input-group-addon" id="awssecretaddon"><a href="http://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSGettingStartedGuide/AWSCredentials.html" target="_blank"><?php echo _("What's this?")?></a></span>
-            					</div>
-            				</div>
-            			</div>
-            		</div>
-            	</div>
-            	<!--END AWS Secret-->
-							<!--Path-->
-							<div class="element-container">
-								<div class="row">
-									<div class="form-group">
-										<div class="col-md-3">
-											<label class="control-label" for="path"><?php echo _("Path") ?></label>
-											<i class="fa fa-question-circle fpbx-help-icon" data-for="path"></i>
-										</div>
-										<div class="col-md-9">
-											<input type="text" class="form-control" id="path" name="path" value="<?php echo isset($path)?$path:''?>"<?php echo $disabled?>>
+						<ul class="nav nav-tabs list" role="tablist">
+							<li data-name="general" class="change-tab active"><a href="#general" aria-controls="general" role="tab" data-toggle="tab"><?php echo _("General") ?></a></li>
+							<li data-name="advanced" class="change-tab"><a href="#advanced" aria-controls="advanced" role="tab" data-toggle="tab"><?php echo _("Advanced") ?></a></li>
+						</ul>
+						<form class="fpbx-submit" action="?display=filestore" method="post" id="server_form" name="server_form" data-fpbx-delete="?display=filestore&action=delete&id=<?php echo isset($_GET['id']) ? $_GET['id'] : '' ?>">
+							<input type="hidden" name="action" value="<?php echo empty($id) ? 'add' : 'edit' ?>">
+							<input type="hidden" name="id" value="<?php echo $id ?>">
+							<input type="hidden" name="driver" value="S3">
+							<div class="tab-content">
+								<div role="tabpanel" class="tab-pane active" id="general">
+									<!--Local Display Name-->
+									<div class="element-container">
+										<div class="row">
+											<div class="form-group">
+												<div class="col-md-3">
+													<label class="control-label" for="name"><?php echo _("Local Display Name") ?></label>
+												</div>
+												<div class="col-md-9">
+													<input type="text" class="form-control" id="name" name="name" value="<?php echo isset($name) ? $name : '' ?>" <?php echo $disabled ?>>
+												</div>
+											</div>
 										</div>
 									</div>
-								</div>
-								<div class="row">
-									<div class="col-md-12">
-										<span id="path-help" class="help-block fpbx-help-block"><?php echo _("Path on remote server")?></span>
+									<!--Bucket Name-->
+									<div class="element-container">
+										<div class="row">
+											<div class="form-group">
+												<div class="col-md-3">
+													<label class="control-label" for="bucket"><?php echo _("Bucket Name") ?></label>
+												</div>
+												<div class="col-md-9">
+													<input type="text" class="form-control" id="bucket" name="bucket" value="<?php echo isset($bucket) ? $bucket : '' ?>" <?php echo $disabled ?>>
+												</div>
+											</div>
+										</div>
 									</div>
+									<!--Description-->
+									<div class="element-container">
+										<div class="row">
+											<div class="form-group">
+												<div class="col-md-3">
+													<label class="control-label" for="desc"><?php echo _("Description") ?></label>
+												</div>
+												<div class="col-md-9">
+													<input type="text" class="form-control" id="desc" name="desc" value="<?php echo isset($desc) ? $desc : '' ?>" <?php echo $disabled ?>>
+												</div>
+											</div>
+										</div>
+									</div>
+									<!--END Description-->
+									<!--AWS Region-->
+									<div class="element-container">
+										<div class="row">
+											<div class="form-group">
+												<div class="col-md-3">
+													<label class="control-label" for="region"><?php echo _("AWS Region") ?></label>
+													<i class="fa fa-question-circle fpbx-help-icon" data-for="region"></i>
+												</div>
+												<div class="col-md-9">
+													<select class="form-control" id="region" name="region">
+														<?php
+														foreach ($regions as $value => $key) {
+															$selected = ($key == $region) ? 'SELECTED' : '';
+															echo '<option value = "' . $key . '" ' . $selected . '>' . $value . ' [' . $key . ']</option>';
+														}
+														?>
+													</select>
+												</div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-12">
+												<span id="region-help" class="help-block fpbx-help-block"><?php echo _("Region provided in AWS") ?></span>
+											</div>
+										</div>
+									</div>
+									<!--END AWS Region-->
+									<!--AWS Access Key-->
+									<div class="element-container">
+										<div class="row">
+											<div class="form-group">
+												<div class="col-md-3">
+													<label class="control-label" for="awsaccesskey"><?php echo _("AWS Access Key") ?></label>
+												</div>
+												<div class="col-md-9">
+													<div class="input-group">
+														<input type="text" class="form-control" id="awsaccesskey" name="awsaccesskey" value="<?php echo isset($awsaccesskey) ? $awsaccesskey : '' ?>" <?php echo $disabled ?>>
+														<span class="input-group-addon" id="awskeyaddon"><a href="http://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSGettingStartedGuide/AWSCredentials.html" target="_blank"><?php echo _("What's this?") ?></a></span>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<!--END AWS Access Key-->
+									<!--AWS Secret-->
+									<div class="element-container">
+										<div class="row">
+											<div class="form-group">
+												<div class="col-md-3">
+													<label class="control-label" for="awssecret"><?php echo _("AWS Secret") ?></label>
+												</div>
+												<div class="col-md-9">
+													<div class="input-group">
+														<input type="text" class="form-control" id="awssecret" name="awssecret" value="<?php echo isset($awssecret) ? $awssecret : '' ?>" <?php echo $disabled ?>>
+														<span class="input-group-addon" id="awssecretaddon"><a href="http://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSGettingStartedGuide/AWSCredentials.html" target="_blank"><?php echo _("What's this?") ?></a></span>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<!--END AWS Secret-->
+									<!--Path-->
+									<div class="element-container">
+										<div class="row">
+											<div class="form-group">
+												<div class="col-md-3">
+													<label class="control-label" for="path"><?php echo _("Path") ?></label>
+													<i class="fa fa-question-circle fpbx-help-icon" data-for="path"></i>
+												</div>
+												<div class="col-md-9">
+													<input type="text" class="form-control" id="path" name="path" value="<?php echo isset($path) ? $path : '' ?>" <?php echo $disabled ?>>
+												</div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-12">
+												<span id="path-help" class="help-block fpbx-help-block"><?php echo _("Path on remote server") ?></span>
+											</div>
+										</div>
+									</div>
+									<!--END Path-->
 								</div>
-							</div>
-							<!--END Path-->
-            </form>
+								<div role="tabpanel" class="tab-pane" id="advanced">
+									<div class="panel panel-warning">
+										<div class="panel-heading">
+											<h3 class="panel-title"><?php echo _("Danger") ?></h3>
+										</div>
+										<div class="panel-body">
+											<?php echo _("Settings here will override settings in the general tab. Only make changes here if you know what you are doing. These settings are not validated and may break functionality!"); ?>
+										</div>
+									</div>
+									<!--Custom Endpoint-->
+									<div class="element-container">
+										<div class="row">
+											<div class="form-group">
+												<div class="col-md-3">
+													<label class="control-label" for="customendpoint"><?php echo _("Custom Endpoint") ?></label>
+													<i class="fa fa-question-circle fpbx-help-icon" data-for="customendpoint"></i>
+												</div>
+												<div class="col-md-9">
+													<input type="text" class="form-control" id="customendpoint" name="customendpoint" value="<?php echo $customendpoint ?>">
+												</div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-12">
+												<span id="customendpoint-help" class="help-block fpbx-help-block"><?php echo _("Custom S3 compatable endpoint") ?></span>
+											</div>
+										</div>
+									</div>
+									<!--END Custom Endpoint-->
+									<!--Custom Region-->
+									<div class="element-container">
+										<div class="row">
+											<div class="form-group">
+												<div class="col-md-3">
+													<label class="control-label" for="customregion"><?php echo _("Custom Region") ?></label>
+													<i class="fa fa-question-circle fpbx-help-icon" data-for="customregion"></i>
+												</div>
+												<div class="col-md-9">
+													<input type="text" class="form-control" id="customregion" name="customregion" value="<?php echo $customregion ?>">
+												</div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-12">
+												<span id="customregion-help" class="help-block fpbx-help-block"><?php echo _("Custom Region") ?></span>
+											</div>
+										</div>
+									</div>
+									<!--END Custom Region-->
+								</div>
+						</form>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-  <p class ="pull-right"> <a href="http://aws.amazon.com" target='_blank'>AWS&trade; and S3&trade; are trademarks of Amazon Web Services Inc</a></p>
+	<p class="pull-right"> <a href="http://aws.amazon.com" target='_blank'>AWS&trade; and S3&trade; are trademarks of Amazon Web Services Inc</a></p>
 </div>
 <script type="text/javascript">
-  var immortal = <?php echo (isset($immortal) && !empty($immortal))?'true':'false';?>;
+	var immortal = <?php echo (isset($immortal) && !empty($immortal)) ? 'true' : 'false'; ?>;
 	$('#server_form').on('submit', function(e) {
-			if($("#bucket").val().length === 0 ) {
-				warnInvalid($("#host"),_("The host cannot be empty"));
-				return false;
-			}else{
-				return true;
-			}
+		if ($("#bucket").val().length === 0) {
+			warnInvalid($("#host"), _("The host cannot be empty"));
+			return false;
+		} else {
+			return true;
+		}
 	});
 </script>
